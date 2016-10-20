4cff27b2b7e78eff334669fde81761b5010d2b34
wangxuan <wangxuan@qgpic.com>
2016-10-20 16:57:15 +0800

    fix a bug

---
+++ src/Providers/TagServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+class TagServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                realpath(__DIR__.'/../../database/migrations') => database_path('migrations')
+            ], 'migrations');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        //
+    }
+
+}
