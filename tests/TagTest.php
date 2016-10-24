5f31915eabc889afb3f2588a3734ce3eb9151a23
wangxuan <wangxuan@qgpic.com>
2016-10-24 11:53:57 +0800

    add phpunit test

---
+++ tests/TagTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Fuguevit\Tags\Tests;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+class TagTest extends TestCase
+{
+    protected $tag;
+
+    public function testTag()
+    {
+        $this->assertTrue(true);
+    }
+}
