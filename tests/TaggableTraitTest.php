9496453b049304ea87cd08a676e3dddf223597e3
wangxuan <wangxuan@qgpic.com>
2016-11-07 16:14:37 +0800

    bumped

--- tests/TaggableTraitTest.php
+++ tests/TaggableTraitTest.php
@@ -4,9 +4,23 @@ namespace Fuguevit\Tags\Tests;
 
 class TaggableTraitTest extends TestCase
 {
+    /**
+     * Taggable can add tag.
+     */
     public function test_it_can_add_tag()
     {
-        return $this->assertTrue(true);
+        $article = $this->createArticle();
+        $article2 = $this->createArticle();
+
+        $article->tag('chinese');
+        $article2->tag(['chinese']);
+
+        $article->fresh();
+        $article2->fresh();
+
+//        dd($article->tags->pluck('slug')->toArray());
+        $this->assertSame(['chinese'], $article->tags->pluck('name')->toArray());
+        $this->assertSame(['chinese'], $article2->tags->pluck('name')->toArray());
     }
 
 }
